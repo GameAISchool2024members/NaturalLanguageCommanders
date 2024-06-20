370e72bc245cffbe763698883c04f03a1e55c794
tonkats <49250987+tonkats@users.noreply.github.com>
2024-06-20 17:21:45 +0200

    Hello world update

--- Assets/Scripts/GPTController.cs
+++ Assets/Scripts/GPTController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using OpenAI_API.Chat;
 using System;
 using OpenAI_API;
+using System.Text;
+using System.Collections.Specialized;
+using OpenAI_API.Moderation;
+using OpenAI_API.Models;
 
 public class GPTController : MonoBehaviour
 {
@@ -12,8 +16,29 @@ public class GPTController : MonoBehaviour
      */
     void Start()
     {
-        var api = new OpenAI_API.OpenAIAPI("YOUR_API_KEY");
-        var result = api.Chat.CreateChatCompletionAsync("Hello world!");
-        Console.WriteLine(result);
+        var api = new OpenAIAPI("KEY HERE");
+
+        var chat = api.Chat.CreateConversation();
+        chat.Model = Model.ChatGPTTurbo;
+        chat.RequestParameters.Temperature = 0;
+
+        /// give instruction as System
+        chat.AppendSystemMessage("You are a teacher who helps children understand if things are animals or not.  If the user tells you an animal, you say \"yes\".  If the user tells you something that is not an animal, you say \"no\".  You only ever respond with \"yes\" or \"no\".  You do not say anything else.");
+
+        // give a few examples as user and assistant
+        chat.AppendUserInput("Is this an animal? Cat");
+        chat.AppendExampleChatbotOutput("Yes");
+        chat.AppendUserInput("Is this an animal? House");
+        chat.AppendExampleChatbotOutput("No");
+
+        // now let's ask it a question
+        addUserInput(chat, "Is this an animal? Dog");
+        addUserInput(chat, "Is this an animal? Chair");
+    }
+
+    async void addUserInput(Conversation chat, String message) {
+        chat.AppendUserInput("Is this an animal? Chair");
+        string response = await chat.GetResponseFromChatbotAsync();
+        Debug.Log(response);
     }
 }
