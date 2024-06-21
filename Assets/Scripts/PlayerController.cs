81fe45a243b450785bead8a64bca764195cfa11e
Oleg Jarma Montoya <ojarmam@unal.edu.co>
2024-06-21 13:13:48 +0200

    update PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,21 +11,14 @@ public class PlayerController : MonoBehaviour
 
     void Update ()
     {
-<<<<<<< Updated upstream
-        gridMovement.DrawMap();
-=======
         
->>>>>>> Stashed changes
         if (Input.GetMouseButtonDown(0))
         {
             // Debug.Log(IsHighlightedTileClicked(mouseWorldPosition));
             // Debug.Log("Clicked on tile: " + highlightedTilePosition);
             tileSelection.goal.transform.position = tileSelection.tilemap.GetCellCenterWorld(tileSelection.GetHighlightedTile());
             gridMovement.search.GenerateMap();
-<<<<<<< Updated upstream
-=======
             gridMovement.DrawMap();
->>>>>>> Stashed changes
             // gridMovement.FollowGoal();
         }
     }
